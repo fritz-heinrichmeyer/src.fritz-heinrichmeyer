3642b990004c18096b73512d662b431c2aecc804
Fritz Heinrichmeyer <fritz.heinrichmeyer@gmail.com>
2017-09-20 09:35:47 +0200

    impressum

--- conf.py
+++ conf.py
@@ -871,8 +871,14 @@ LICENSE = ""
 
 # A small copyright notice for the page footer (in HTML).
 # (translatable)
-CONTENT_FOOTER = 'Contents &copy; {date}         <a href="mailto:{email}">{author}</a> - Powered by         <a href="https://getnikola.com" rel="nofollow">Nikola</a>         {license}'
+# CONTENT_FOOTER = 'Contents &copy; {date}         <a href="mailto:{email}">{author}</a> - Powered by         <a href="https://getnikola.com" rel="nofollow">Nikola</a>         {license}'
+CONTENT_FOOTER = """<div class="text-center">
+<div class="siegel"><table>
 
+</table></div>
+</div>
+<p><a href="/pages/impressum/" rel="nofollow">Impressum</a> - Contents &copy; {date}         <a href="mailto:{email}">{author}</a> - Powered by         <a href="https://getnikola.com" rel="nofollow">Nikola </a>          {license}
+</p>"""
 # Things that will be passed to CONTENT_FOOTER.format().  This is done
 # for translatability, as dicts are not formattable.  Nikola will
 # intelligently format the setting properly.
